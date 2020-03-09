7b6d4f68d9e89c124a2b1940e0646eb63f7b2944
Jorge Lopez <jorgekl.1994@gmail.com>
2020-03-09 17:13:42 -0600

    Agregados endpoints

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const port = process.env.PORT || 3001;
-const principalRouter = require("./routes/principal");
+const principalRouter = require('./routes/principal');
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use("/", principalRouter);
+
+app.use('/', principalRouter);
+
 app.listen(port, function () {
-    console.log("Runnning on " + port);
+    console.log("Esperando en " + port);
 });
 
 module.exports = app;
--- routes/principal.js
+++ routes/principal.js
@@ -1,15 +1,28 @@
 const express = require("express");
 const router = express.Router();
+const mysql = require('mysql');
 
-router.post("/", async (req, res) => {
-    res.status(200).json({
-        "msg": "hola"
+const conn = mysql.createPool({
+    host: 'us-cdbr-iron-east-04.cleardb.net',
+    user: 'bd4b1aaf518d86',
+    password: '6b1e5ab4',
+    database: 'heroku_af73cab8a038b48'
+});
+
+router.post("/", (req, res) => {
+    const { sensor, valor, fecha, hora } = req.body;
+    const date = fecha.split('.').join('-');
+    const time = hora.split('.').join(':');
+    const query = `INSERT INTO DatoSensor (valor, fecha, hora, id) VALUES ('${valor}', '${date}', '${time}', '${sensor}')`;
+    const sql = conn.query(query, (err, results) => {
+        res.json();
     });
 });
 
-router.get("/", async (req, res) => {
-    res.status(200).json({
-        "msg": "hola"
+router.get("/", (req, res) => {
+    const query = `SELECT valor, fecha, hora, nombre FROM TipoSensor, DatoSensor WHERE TipoSensor.id=DatoSensor.id;`;
+    const sql = conn.query(query, (err, results) => {
+        res.json(results);
     });
 });
 
